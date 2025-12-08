0304f4e5496e1ee3c48fb7bd25f86a67142ff78f
Tanishka Sharma <anjanasharma8448766174@gmail.com>
2025-12-08 13:27:38 +0530

    changes made in auth route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -79,7 +79,7 @@ authRouter.get("/profile", async (req, res) => {
   if (!token) return res.status(401).json({ message: "No token provided" });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
     const user = await userModel
       .findById(decoded.id)
