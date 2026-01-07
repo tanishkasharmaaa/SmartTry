c87f6823c90f395820a79960bb65f3d8a30f9183
Tanishka Sharma <anjanasharma8448766174@gmail.com>
2026-01-07 06:15:57 +0530

    redis changes added

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,10 +1,10 @@
-// config/redis.js
 require("dotenv").config();
 const Redis = require("ioredis");
 
 const redis = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,  // avoid MaxRetriesPerRequestError
   enableOfflineQueue: true,    // queue jobs if Redis temporarily down
+  tls: {} // ensure TLS is enabled for rediss://
 });
 
 redis.on("connect", () => console.log("✅ Redis connected"));
